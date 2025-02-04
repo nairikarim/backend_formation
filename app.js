24f9040a43387fbbb4f5dbb530c809c99e4876a6
nairikarim <nairikarim50@gmail.com>
2025-02-04 20:42:10 +0100

    configuration du projet

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const http =require('http'); //1
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -38,4 +36,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+const server = http.createServer(app); //2
+ server.listen(5000 ,() => {
+  console.log("app is running on port 5000") ;
+ });
